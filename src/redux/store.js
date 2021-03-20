733d3339da081d9e9e6639554aef8e13b7fcfae2
Andrew <andrew.bingham.dev@gmail.com>
2021-03-20 17:09:53 +0000

    State now persists using localStorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,20 @@ import { createStore, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import { favouritesReducer } from './favourites/reducers';
+import { loadState, saveState } from './localStorage';
 
 const rootReducer = combineReducers({
 	favourites: favouritesReducer,
 });
 
-const store = createStore(rootReducer, composeWithDevTools());
+const initialState = loadState();
+console.log('initState: ', initialState);
+const store = createStore(rootReducer, initialState, composeWithDevTools());
+
+store.subscribe(() => {
+	saveState({
+		favourites: store.getState().favourites,
+	});
+});
 
 export default store;
